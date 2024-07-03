19a790efb5bef0194f6970c1933819d4b38652ff
Xuan Thoai <thoailata1000@gmail.com>
2024-07-03 09:22:10 +0700

    Modify

--- app.py
+++ app.py
@@ -1,6 +1,52 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+import json
+
 app = Flask(__name__)
+
+PAGE_ACCESS_TOKEN = "EAARynmWpSZCIBOZBOZCSO0ybd4BB4ZAkOZB7IVu1l7OAQXt9FJTc3dbcEnJqogORqKjLkJWQCge4AZBMoiKlZCgHVKZAJjZB6bZArVaEreFK6TBpqsP6RYZBagLLKNTz6eZCUOi2bXboQuZB3zI1AuzgzAFJfMiyz2OqZBQs3lSYeuBFn7gpgXU5mmta4uzZC9wQCEqj5H2dBI99ZCnTK70PV1tmSgZDZD"
+sender_id = "7670798873002030"
+customer_id = "PC08CC0332738"
+reqAuth = {
+    "url": f"https://graph.facebook.com/v20.0/me/messages?access_token={PAGE_ACCESS_TOKEN}",
+    "login": "https://cskh-api.cpc.vn/api/cskh/user/login",
+    "data": "https://cskh-api.cpc.vn/api/remote/meter/rf/sl-tieu-thu-view",
+    "payment": "https://appcskh.cpc.vn:4433/api/v4/customer/home/",
+    "postmethod": "POST",
+    "getMethod": "GET"
+}
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Extract username and password from the request
+    #data = request.json
+
+
+    body = json.dumps({
+        "username": "PC08CC0332738",
+        "password": "Kurata1909!",
+        "grant_type": "password",
+        "scope": "CSKH",
+        "ThongTinCaptcha": {
+            "captcha": "undefined",
+            "token": "undefined"
+        }
+    })
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Content-Type": "application/json;charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
+        "Connection": "keep-alive"
+    }
+
+    # Make the HTTP request
+    response = requests.post(reqAuth['login'], headers=headers, data=body)
+
+    # Return the response from the external service
+    return jsonify(response.json()), response.status_code
